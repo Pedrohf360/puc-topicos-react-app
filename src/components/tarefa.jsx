b05076f6b117cc4af3cdb1da4015593e5bffc06e
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2020-06-14 23:36:47 -0300

    Add tarefa component
    Add tarefa.jsx component
    Add fontAwesome import

---
+++ src/components/tarefa.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Tarefa extends Component {
+    state = {
+        descricao: 'Tarefa',
+    };
+    render() {
+        return <li className="list-group-item">{this.state.descricao}</li>;
+    }
+}
+
+export default Tarefa;
--- src/components/tarefas.jsx
+++ src/components/tarefas.jsx
@@ -1,68 +1,74 @@
 import React, { Component } from 'react';
+import Tarefa from './tarefa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './tarefas.css';
 
 class Tarefas extends Component {
-  state = {
-    tarefas: ['Declarar IRPF', 'Estudar React', 'Levar o carro para a revisão'],
-    novaTarefa: '',
-  };
+    state = {
+        tarefas: ['Declarar IRPF', 'Estudar React', 'Levar o carro para a revisão'],
+        novaTarefa: '',
+    };
 
-  render() {
-    return (
-      <div>
-        <h2>Tarefas pendentes</h2>
-        <p>{this.tarefasPendentes()}</p>
-        {this.listaDeTarefas()}
-        <form className="form-inline mt-4" onSubmit={this.adicionaTarefa}>
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.novaTarefa}
-              onChange={this.trataNovaTarefa}
-            />
-            <button className="btn btn-secondary" type="submit">
-              +
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Tarefas pendentes</h2>
+                <p>{this.tarefasPendentes()}</p>
+                {this.listaDeTarefas()}
+                <form className="form-inline mt-4" onSubmit={this.adicionaTarefa}>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.novaTarefa}
+                            onChange={this.trataNovaTarefa}
+                        />
+                        <span className="input-group-append">
+                            <button className="btn btn-secondary" type="submit">
+                                <FontAwesomeIcon icon={faPlus} />
+                            </button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 
-  trataNovaTarefa = (evento) => {
-    this.setState({ novaTarefa: evento.target.value });
-  };
+    trataNovaTarefa = (evento) => {
+        this.setState({
+            novaTarefa: evento.target.value,
+        });
+    };
 
-  adicionaTarefa = (evento) => {
-    const novoVetorDeTarefas = [...this.state.tarefas, this.state.novaTarefa];
-    this.setState({
-      tarefas: novoVetorDeTarefas,
-    });
-    evento.preventDefault();
-  };
+    adicionaTarefa = (evento) => {
+        const novoVetorDeTarefas = [...this.state.tarefas, this.state.novaTarefa];
+        this.setState({
+            tarefas: novoVetorDeTarefas,
+            novaTarefa: '',
+        });
+        evento.preventDefault();
+    };
 
-  tarefasPendentes() {
-    const { tarefas } = this.state;
-    if (tarefas.length > 1) return tarefas.length + ' tarefas pendentes.';
-    else if (tarefas.length === 1) return '1 tarefa pendente.';
-    else return 'Nenhuma tarefa pendente.';
-  }
+    tarefasPendentes() {
+        const { tarefas } = this.state;
+        if (tarefas.length > 1) return tarefas.length + ' tarefas pendentes.';
+        else if (tarefas.length === 1) return '1 tarefa pendente.';
+        else return 'Nenhuma tarefa pendente.';
+    }
 
-  listaDeTarefas() {
-    const { tarefas } = this.state;
-    return (
-      tarefas.length > 0 && (
-        <ul className="list-group mt-4">
-          {tarefas.map((t, i) => (
-            <li className="list-group-item" key={i}>
-              {t}
-            </li>
-          ))}
-        </ul>
-      )
-    );
-  }
+    listaDeTarefas() {
+        const { tarefas } = this.state;
+        return (
+            tarefas.length > 0 && (
+                <ul className="list-group mt-4">
+                    {tarefas.map((t, i) => (
+                        <Tarefa key={i} />
+                    ))}
+                </ul>
+            )
+        );
+    }
 }
 
 export default Tarefas;
