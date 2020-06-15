2eaf530e917115d8a28529c304e4d92e5f864a9a
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2020-06-14 23:18:35 -0300

    Update tarefas.jsx
    Add setState to update front.

--- src/components/tarefas.jsx
+++ src/components/tarefas.jsx
@@ -1,46 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './tarefas.css';
 
 class Tarefas extends Component {
-    state = {
-        tarefas: [
-            'Declarar IRPF',
-            'Estudar React',
-            'Levar bicicleta para revisão'
-        ]
-    };
-    imagem = 'https://via.placeholder.com/250x100.png';
-    // estilo = {fontFamily: 'Impacto, Calibri, sans-serif', color: '#F00'};
+  state = {
+    tarefas: ['Declarar IRPF', 'Estudar React', 'Levar o carro para a revisão'],
+    novaTarefa: '',
+  };
 
-    render() {
-        return(
-            <div>
-                <h2 style={this.estilo}>Tarefas pendentes</h2>
-                <p>{this.getQuantTarefasPendentes()}</p>
-                <ul>{this.getListaTarefasPendentes()}</ul>
-                <p>
-                    <img src={this.imagem} alt="placeholder"/>
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Tarefas pendentes</h2>
+        <p>{this.tarefasPendentes()}</p>
+        {this.listaDeTarefas()}
+        <form className="form-inline mt-4" onSubmit={this.adicionaTarefa}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.novaTarefa}
+              onChange={this.trataNovaTarefa}
+            />
+            <button className="btn btn-secondary" type="submit">
+              +
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 
-    getQuantTarefasPendentes() {
-        if (this.state.tarefas.length === 1) 
-            return `Você possui 1 tarefa pendente`;
-        else if (this.state.tarefas.length > 1) 
-            return `Você possui ${this.state.tarefas.length} tarefas pendentes`;
-        else
-            return `Nenhuma tarefa pendente foi encontrada.`;
-    }
+  trataNovaTarefa = (evento) => {
+    this.setState({ novaTarefa: evento.target.value });
+  };
 
-    getListaTarefasPendentes() {
-        return (
-            this.state.tarefas.map((t, i) => {
-                return (<li key={i}>{t}</li>);
-            })
-        );
-    }
+  adicionaTarefa = (evento) => {
+    const novoVetorDeTarefas = [...this.state.tarefas, this.state.novaTarefa];
+    this.setState({
+      tarefas: novoVetorDeTarefas,
+    });
+    evento.preventDefault();
+  };
+
+  tarefasPendentes() {
+    const { tarefas } = this.state;
+    if (tarefas.length > 1) return tarefas.length + ' tarefas pendentes.';
+    else if (tarefas.length === 1) return '1 tarefa pendente.';
+    else return 'Nenhuma tarefa pendente.';
+  }
+
+  listaDeTarefas() {
+    const { tarefas } = this.state;
+    return (
+      tarefas.length > 0 && (
+        <ul className="list-group mt-4">
+          {tarefas.map((t, i) => (
+            <li className="list-group-item" key={i}>
+              {t}
+            </li>
+          ))}
+        </ul>
+      )
+    );
+  }
 }
 
 export default Tarefas;
